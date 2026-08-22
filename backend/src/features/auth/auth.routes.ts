6cc3d3a32961d89ff040228f4472eb84971c8ca4
jvke <shubhambrown1@gmail.com>
2026-08-22 11:54:29 +0530

    feat(auth) implement rate limiting in backend on login and register routes

--- backend/src/features/auth/auth.routes.ts
+++ backend/src/features/auth/auth.routes.ts
@@ -3,13 +3,14 @@ import { getMe, login, logout, refresh, register } from "./auth.controller.js";
 import { validate } from "../../shared/middlewares/validate.js";
 import { loginSchema, registerSchema } from "./auth.schema.js";
 import { authenticate } from "../../shared/middlewares/auth.middleware.js";
+import { authLimiter } from "../../shared/middlewares/rateLimiter.js";
 
 const router = Router();
 
-router.post("/register", validate(registerSchema), register);
-router.post("/login", validate(loginSchema), login);
+router.post("/register", authLimiter, validate(registerSchema), register);
+router.post("/login", authLimiter, validate(loginSchema), login);
 router.post("/refresh", refresh);
 router.get("/me", authenticate, getMe);
-router.post("/logout", logout)
+router.post("/logout", logout);
 
 export default router;
