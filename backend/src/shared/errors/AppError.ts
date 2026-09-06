674e31f9f6542e217aae9a90806196f9254993ae
jvke <shubhambrown1@gmail.com>
2026-09-06 13:44:15 +0530

    Improve the AppError (removing unused code)

--- backend/src/shared/errors/AppError.ts
+++ backend/src/shared/errors/AppError.ts
@@ -1,18 +1,10 @@
 export class AppError extends Error {
-  public readonly statusCode: number;
-  public readonly isOperational: boolean;
-
   constructor(
     message: string,
-    statusCode: number = 500,
-    isOperational: boolean = true,
+    public readonly statusCode: number = 500,
+    public readonly isOperational: boolean = true,
   ) {
     super(message);
-
-    this.statusCode = statusCode;
-    this.isOperational = isOperational;
-
-    Object.setPrototypeOf(this, new.target.prototype);
     Error.captureStackTrace(this, this.constructor);
   }
 
